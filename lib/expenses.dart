b06074d81f02f923bdfae6436f59903ecf0da85c
TeeWrath <subroto.2003@gmail.com>
2024-02-26 11:46:36 +0530

    feat: adding expenses (incomplete)

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class ExpensesScreen extends StatefulWidget {
@@ -8,14 +9,24 @@ class ExpensesScreen extends StatefulWidget {
 }
 
 class _ExpensesScreenState extends State<ExpensesScreen> {
+  List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Flutter Course',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: 'Cinema',
+        amount: 200,
+        date: DateTime.now(),
+        category: Category.leisure)
+  ];
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Column(
-        children: [
-          Text('Charts and stuff'),
-          Text('List of expenses...')
-        ],
+        children: [Text('Charts and stuff'), Text('List of expenses...')],
       ),
     );
   }
