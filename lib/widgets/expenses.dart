83cfb3a04d61b317453370bb76012c436cc4ab7e
TeeWrath <subroto.2003@gmail.com>
2024-03-02 14:51:05 +0530

    feat: modal sheet for expense adding, different showboxes as per required and some styling

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/widgets/expense_list/expense_list.dart';
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class ExpensesScreen extends StatefulWidget {
@@ -23,6 +24,10 @@ class _ExpensesScreenState extends State<ExpensesScreen> {
         category: Category.leisure)
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(context: context, builder: (ctx) => const NewExpense());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,7 +35,8 @@ class _ExpensesScreenState extends State<ExpensesScreen> {
         title: const Text('Flutter Expense Tracker'),
         backgroundColor: const Color.fromARGB(255, 103, 39, 176),
         actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.add))
+          IconButton(
+              onPressed: _openAddExpenseOverlay, icon: const Icon(Icons.add))
         ],
       ),
       body: Column(
