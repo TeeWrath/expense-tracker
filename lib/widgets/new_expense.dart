b427e41a1a53d45afb79ee78c49fed2fdf08c69d
TeeWrath <subroto.2003@gmail.com>
2024-03-02 17:10:03 +0530

    feat: dropdownbutton feature added

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -12,6 +12,7 @@ class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
   DateTime? _selectedDate;
+  Category _selectedCategory = Category.leisure;
 
   void _presentDatePicker() async {
     final now = DateTime.now();
@@ -64,7 +65,11 @@ class _NewExpenseState extends State<NewExpense> {
                 mainAxisAlignment: MainAxisAlignment.end,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Text(_selectedDate == null ? "No Date Selected" : formatter.format(_selectedDate!),),
+                  Text(
+                    _selectedDate == null
+                        ? "No Date Selected"
+                        : formatter.format(_selectedDate!),
+                  ),
                   IconButton(
                       onPressed: _presentDatePicker,
                       icon: const Icon(Icons.calendar_month))
@@ -72,8 +77,27 @@ class _NewExpenseState extends State<NewExpense> {
               ))
             ],
           ),
+          const SizedBox(height: 16,),
           Row(
             children: [
+              DropdownButton(
+                value: _selectedCategory,
+                  items: Category.values
+                      .map((category) => DropdownMenuItem(
+                            value: category,
+                            child: Text(category.name.toUpperCase()),
+                          ))
+                      .toList(),
+                  onChanged: (value) {
+                    if (value == null) {
+                      return;
+                    }
+                    // Below code will only execute if the value of value is not null
+                    setState(() {
+                      _selectedCategory = value;
+                    });
+                  }),
+              const Spacer(),
               TextButton(
                   onPressed: () {
                     Navigator.pop(context);
